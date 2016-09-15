1c75e2da8faf31a2a086e316a9d8b05f9ce84129
Josua Krause <josua.krause@gmail.com>
2016-09-15 14:18:44 -0400

    use rst for README

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ from os import path
 
 here = path.abspath(path.dirname(__file__))
 
-with open(path.join(here, 'readme.md'), encoding='utf-8') as f:
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
