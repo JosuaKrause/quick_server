9ddb5608fff6678480fc4e3c846f52e44eeb2195
Josua Krause <josua.krause@gmail.com>
2018-10-18 16:42:15 -0400

    update worker js

--- quick_server/worker.js
+++ quick_server/worker.js
@@ -13,15 +13,15 @@ export const CONFIG = {
   timeCap: 1000*60,
   timeMinInc: 10,
   timeMulInc: 1.01,
-  animation: ["⠋", "⠙", "⠸", "⠴", "⠦", "⠇"],
-  // animation: ["/", "-", "\\", "|"],
+  animation: ['⠋', '⠙', '⠸', '⠴', '⠦', '⠇'],
+  // animation: ['/', '-', '\\', '|'],
   animationTime: 300,
 };
-export const VERSION = "0.5.5";
+export const VERSION = '0.5.6';
 
 export class Worker {
   constructor() {
-    window.addEventListener("beforeunload", () => {
+    window.addEventListener('beforeunload', () => {
       Object.keys(this._tokens).forEach((ref) => {
         // we probably won't read the results but
         // the server still should cancel properly
@@ -58,8 +58,8 @@ export class Worker {
   }
 
   set infoTitle(infoTitle) {
-    if(!infoTitle) {
-      this.setAddTitle("");
+    if (!infoTitle) {
+      this.setAddTitle('');
     }
     this._infoTitle = infoTitle;
   }
@@ -69,12 +69,12 @@ export class Worker {
   }
 
   setAddTitle(addTitle) {
-    if(!this._infoTitle) return;
+    if (!this._infoTitle) return;
     const curTitle = document.title;
-    if(this._ownTitle && curTitle !== this._ownTitle) { // external change
+    if (this._ownTitle && curTitle !== this._ownTitle) { // external change
       this._beforeTitle = curTitle;
     }
-    if(!this._beforeTitle) {
+    if (!this._beforeTitle) {
       this._beforeTitle = curTitle;
     }
     this._ownTitle = this._beforeTitle + addTitle;
@@ -82,31 +82,37 @@ export class Worker {
   } // setAddTitle
 
   sendRequest(url, obj, cb) {
+    const BENIGN_ERR = 'BENIGN_ERR';
     fetch(url, {
-      "method": "POST",
-      "headers": new Headers({
-        "Content-Type": "application/json",
-        "Content-Length": "" + obj.length,
+      method: 'POST',
+      headers: new Headers({
+        'Content-Type': 'application/json',
+        'Content-Length': `${obj.length}`,
       }),
-      "body": obj,
+      body: obj,
     }).then((data) => {
-      if(data.status !== 200 || !data.ok) {
-        console.warn("response not okay", data);
-        throw new Error(data.status + " " + data.statusText);
+      if (data.status !== 200 || !data.ok) {
+        cb({
+          err: data.statusText,
+          code: data.status,
+        }, null);
+        throw new Error(BENIGN_ERR);
       }
-      const ct = data.headers.get("content-type");
-      if(ct && ct.includes("application/json")) {
+      const ct = data.headers.get('content-type');
+      if (ct && ct.includes('application/json')) {
         return data.json();
       }
-      throw new TypeError("response not JSON encoded");
-    }).then((data) => cb(null, data)).catch((e) => cb(e, null));
+      throw new TypeError('response not JSON encoded');
+    }).then((data) => cb(null, data)).catch((e) => {
+      if (e.message !== BENIGN_ERR) cb(e, null);
+    });
   }
 
   changeStatus(inc, error) {
-    if(this._req < 0) return;
-    if(error) {
+    if (this._req < 0) return;
+    if (error) {
       this._req = -1;
-    } else if(inc) {
+    } else if (inc) {
       this._req += 1;
     } else {
       this._req -= 1;
@@ -116,17 +122,17 @@ export class Worker {
   } // changeStatus
 
   titleStatus() {
-    if(this._req <= 0) {
-      this.setAddTitle("");
+    if (this._req <= 0) {
+      this.setAddTitle('');
       return;
     }
     const anim = CONFIG.animation;
-    let txt = " " + anim[this._animationIx % anim.length];
-    if(this._req > 1) {
+    let txt = ` ${anim[this._animationIx % anim.length]}`;
+    if (this._req > 1) {
       txt += ` (${this._req}x)`;
     }
     this.setAddTitle(txt);
-    if(this._infoTitle && !this._animationInFlight) {
+    if (this._infoTitle && !this._animationInFlight) {
       this._animationIx = (this._animationIx + 1) % anim.length;
       this._animationInFlight = true;
       setTimeout(() => {
@@ -136,36 +142,42 @@ export class Worker {
     }
   } // titleStatus
 
-  get_payload(data, url, cb) {
-    if(!data["continue"]) {
-      cb(JSON.parse(data["result"]));
+  getPayload(data, url, cb, errCb) {
+    if (!data.continue) {
+      cb(JSON.parse(data.result));
       return;
     }
-    const keys = data["result"];
+    const keys = data.result;
     const res = {};
     keys.forEach((k) => {
       const obj = JSON.stringify({
-        "action": "cargo",
-        "token": k,
+        action: 'cargo',
+        token: k,
       });
       this.sendRequest(url, obj, (err, data) => {
-        if(err) {
+        if (err) {
           console.warn(`Failed to retrieve cargo ${k}`);
           this.changeStatus(false, true);
-          return console.warn(err);
-        }
-        if(k !== data["token"]) {
-          console.warn(`Mismatching token ${k} !== ${data["token"]}`);
-          this.changeStatus(false, true);
+          console.warn(err);
+          errCb && errCb(err);
           return;
         }
-        res[k] = data["result"];
-        check_finished();
+        if (k !== data.token) {
+          const errMsg = `Mismatching token ${k} !== ${data.token}`;
+          console.warn(errMsg);
+          this.changeStatus(false, true);
+          errCb && errCb({
+            err: errMsg,
+          });
+          return;
+        }
+        res[k] = data.result;
+        checkFinished();
       });
     });
 
-    const check_finished = () => {
-      if(!keys.every((k) => k in res)) {
+    const checkFinished = () => {
+      if (!keys.every((k) => k in res)) {
         return;
       }
       const d = keys.map((k) => res[k]).join(''); // string may be too long :(
@@ -173,92 +185,100 @@ export class Worker {
         res[k] = null;
       });
       cb(JSON.parse(d));
-    } // check_finished
-  } // get_payload
+    }; // checkFinished
+  } // getPayload
 
   postTask(ref) {
     setTimeout(() => {
-      if(!this._starts[ref]) return;
+      if (!this._starts[ref]) return;
       const s = this._starts[ref];
-      const url = s["url"];
-      const cb = s["cb"];
+      const url = s.url;
+      const cb = s.cb;
+      const errCb = s.errCb;
       this._starts[ref] = null;
       this.changeStatus(true, false);
       const obj = JSON.stringify({
-        "action": "start",
-        "payload": s["payload"],
+        action: 'start',
+        payload: s.payload,
       });
       this.sendRequest(url, obj, (err, data) => {
-        if(err) {
+        if (err) {
           console.warn(`Failed to start ${ref}`);
           this.changeStatus(false, true);
-          return console.warn(err);
+          console.warn(err);
+          errCb && errCb(err);
+          return;
         }
         this._cancel(ref, (err) => {
-          if(err) {
+          if (err) {
             console.warn(`Failed to cancel ${ref}`);
             this.changeStatus(false, true);
             return console.warn(err);
           }
         });
-        if(data["done"]) {
-          this.get_payload(data, url, (d) => {
+        if (data.done) {
+          this.getPayload(data, url, (d) => {
             this.execute(cb, d);
-          });
+          }, errCb);
         } else {
-          const token = +data["token"];
+          const token = +data.token;
           this._urls[ref] = url;
           this._tokens[ref] = token;
-          this.monitor(ref, token, cb, CONFIG.timeStart);
+          this.monitor(ref, token, cb, errCb, CONFIG.timeStart);
         }
       });
     }, CONFIG.preDelay);
   } // postTask
 
-  monitor(ref, token, cb, delay) {
-    if(this._tokens[ref] !== token) {
+  monitor(ref, token, cb, errCb, delay) {
+    if (this._tokens[ref] !== token) {
       this.changeStatus(false, false);
       return;
     }
     const url = this._urls[ref];
     const obj = JSON.stringify({
-      "action": "get",
-      "token": token,
+      action: 'get',
+      token: token,
     });
     this.sendRequest(url, obj, (err, data) => {
-      if(err) {
+      if (err) {
         console.warn(`Error while retrieving ${ref} token: ${token}`);
         this.changeStatus(false, true);
-        return console.warn(err);
+        console.warn(err);
+        errCb && errCb(err);
+        return;
       }
-      const cur_token = +data["token"];
-      if(cur_token !== this._tokens[ref]) {
+      const curToken = +data.token;
+      if (curToken !== this._tokens[ref]) {
         // late response
         this.changeStatus(false, false);
         return;
       }
-      if(cur_token !== token) {
+      if (curToken !== token) {
         // wrong response
         console.warn(`Error while retrieving ${ref}`);
         this.changeStatus(false, true);
-        return console.warn({
-          "err": `token mismatch: ${cur_token} instead of ${token}`,
-        });
+        const errData = {
+          err: `token mismatch: ${curToken} instead of ${token}`,
+        };
+        console.warn(errData);
+        errCb && errCb(errData);
+        return;
       }
-      if(data["done"]) {
+      if (data.done) {
         this._tokens[ref] = -1;
         this._urls[ref] = null;
-        this.get_payload(data, url, (d) => {
+        this.getPayload(data, url, (d) => {
           this.execute(cb, d);
         });
-      } else if(data["continue"]) {
+      } else if (data.continue) {
         setTimeout(() => {
           const newDelay = Math.min(
             Math.max(
               delay * CONFIG.timeMulInc, delay + CONFIG.timeMinInc
             ), CONFIG.timeCap
           );
-          this.monitor(ref, token, cb, newDelay);
+          this.monitor(ref, token, cb, errCb, newDelay);
         }, delay);
       } else {
         this.changeStatus(false, false);
@@ -267,21 +287,21 @@ export class Worker {
   } // monitor
 
   _cancel(ref, cb) {
-    if(!(ref in this._tokens && this._tokens[ref] >= 0)) return;
+    if (!(ref in this._tokens && this._tokens[ref] >= 0)) return;
     const token = this._tokens[ref];
     const url = this._urls[ref];
     const obj = JSON.stringify({
-      "action": "stop",
-      "token": token,
+      action: 'stop',
+      token: token,
     });
     this._tokens[ref] = -1;
     this._urls[ref] = null;
     this.sendRequest(url, obj, (err, data) => {
-      if(err) {
+      if (err) {
         return cb(err);
       }
-      return cb(+data["token"] !== token && {
-        "err": `token mismatch: ${data["token"]} instead of ${token}`,
+      return cb(+data['token'] !== token && {
+        err: `token mismatch: ${data['token']} instead of ${token}`,
       });
     });
   } // _cancel
@@ -292,7 +312,7 @@ export class Worker {
       cb(data);
       err = false;
     } finally {
-      if(err) {
+      if (err) {
         this.changeStatus(false, true);
       } else {
         this.changeStatus(false, false);
@@ -300,12 +320,13 @@ export class Worker {
     }
   } // execute
 
-  post(ref, url, payload, cb) {
-    if(!this._active) return;
+  post(ref, url, payload, cb, errCb) {
+    if (!this._active) return;
     this._starts[ref] = {
-      "url": url,
-      "cb": cb,
-      "payload": payload,
+      url: url,
+      cb: cb,
+      errCb: errCb,
+      payload: payload,
     };
     this.postTask(ref);
   } // post
@@ -313,7 +334,7 @@ export class Worker {
   cancel(ref) {
     this._cancel(ref, (err) => {
       this.changeStatus(false, !!err);
-      if(err) {
+      if (err) {
         console.warn(`Failed to cancel ${ref}`);
         return console.warn(err);
       }
