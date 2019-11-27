f5b180b30c265c91e9d1dd00c5c384e8604ce4aa
Josua Krause <josua.krause@gmail.com>
2019-11-26 21:50:08 -0500

    correct version

--- quick_server/quick_server.py
+++ quick_server/quick_server.py
@@ -156,7 +156,7 @@ def get_time() -> float:
     return time.monotonic()
 
 
-__version__ = "0.7.0"
+__version__ = "0.7.1"
 
 
 def _getheader_fallback(obj: Any, key: str) -> Any:
--- quick_server/worker.legacy.js
+++ quick_server/worker.legacy.js
@@ -18,7 +18,7 @@ window.CONFIG = {
   // animation: ['/', '-', '\\', '|'],
   animationTime: 300,
 };
-window.VERSION = '0.7.0';
+window.VERSION = '0.7.1';
 
 class Worker {
   constructor() {
