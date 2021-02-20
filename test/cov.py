9c5b5dcb5ae6c4b87c42140cf61e15981bf8b6c5
Josua Krause <josua.krause@gmail.com>
2021-02-20 00:14:56 -0500

    pylint

--- test/cov.py
+++ test/cov.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 try:
-    import coverage
+    import coverage  # pylint: disable=import-error
     coverage.process_startup()
 # pylint: disable=bare-except
 except:  # nopep8
