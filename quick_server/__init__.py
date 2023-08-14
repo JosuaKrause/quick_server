98f13c0b22805c5584012b0e58fb0edb07da8465
Josua Krause <josua.krause@gmail.com>
2023-08-14 16:24:21 -0400

    standard find

--- quick_server/__init__.py
+++ quick_server/__init__.py
@@ -1,3 +1,4 @@
 from .quick_server import *
-from .quick_server import __version__
-from .worker_request import WorkerError, worker_request
+from .quick_server import __version__ as __version__
+from .worker_request import WorkerError as WorkerError
+from .worker_request import worker_request as worker_request
